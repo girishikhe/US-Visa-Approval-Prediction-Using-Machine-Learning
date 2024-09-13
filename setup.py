360f1d7b70c8c015b9d204351a5410696a35e723
Girish <girish.ikhe.vu@gmail.com>
2024-09-13 09:11:45 +0530

    all dependencies installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Girish",
+    author_email="girish.ikhe.@gmail.com",
+    packages=find_packages()
+)
