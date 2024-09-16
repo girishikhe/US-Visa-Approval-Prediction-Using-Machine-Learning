3476ca814d44efb01b5f87d2e059034ed5f82273
Girish <girish.ikhe.vu@gmail.com>
2024-09-16 11:24:38 +0530

    data ingestion is completed

--- demo.py
+++ demo.py
@@ -1,13 +1,4 @@
-from us_visa.logger import logging
+from us_visa.pipline.training_pipeline import TrainPipeline
 
-from us_visa.exception import USvisaException
-import sys
-
-logging.info("Welcome to custom log")
-
-
-
-try:
-    a=2/0
-except Exception as e:
-    raise USvisaException(e,sys)
+obj = TrainPipeline()
+obj.run_pipeline()
